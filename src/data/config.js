c758691cb7edd12492625f3a0c9fc8c454493ed4
Pobepto <roobertdantes@gmail.com>
2019-05-29 16:08:48 +0300

    chore: added license

--- src/data/config.js
+++ src/data/config.js
@@ -1,3 +1,7 @@
+/*
+ * Copyright D3 Ledger, Inc. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
 const NOTARY_ACCOUNT = process.env.VUE_APP_NOTARY_ACCOUNT || 'notary@notary'
 const RELAY_ACCOUNT_KEY = process.env.VUE_APP_RELAY_ACCOUNT_KEY || 'eth_registration_service@notary'
 
